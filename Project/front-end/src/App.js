9776c80f537e8801d2f940d74040ce691a64aefc
shallow960 <dksgudwn45@gmail.com>
2025-07-14 18:27:02 +0900

    dad

--- Project/front-end/src/App.js
+++ Project/front-end/src/App.js
@@ -1,26 +1,11 @@
-import logo from './common/image/main/dp_logo.png';
 import './App.css';
 import './layout/main/css/main.css';
 
+// src/App.jsx
+import MainLayout from './layout/MainLayout';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <MainLayout />;
 }
 
 export default App;
