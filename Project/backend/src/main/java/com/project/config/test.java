f63fa18111db327268a7edae57b5ea58620893f7
shallow960 <dksgudwn45@gmail.com>
2025-07-15 11:02:31 +0900

    code test file upload

---
+++ Project/backend/src/main/java/com/project/config/test.java
@@ -0,0 +1,11 @@
+package com.project.config;
+
+public class test {
+	
+//	config 보안관련
+}
+
+//설정관련 클래스 모아두는 폴더 - 설정센터
+//전역설정, 모듈설정, 외부 라이브러리 설정 디렉토리
+
+//주요 애너테이션	@Configuration, @EnableWebMvc, @Bean 등
