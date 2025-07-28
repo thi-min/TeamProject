7a8a9af19e73feb2c35e81b45ed6517669244e2f
thi-min <lovemin712@gmail.com>
2025-07-28 10:36:35 +0900

    ad

--- Project/backend/src/main/java/com/project/admin/dto/AdminMemberStateUpdateRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.project.admin.dto;
-
-import com.project.member.entity.MemberState;
-import jakarta.validation.constraints.NotNull;
-
-public class AdminMemberStateUpdateRequestDto{
-  @NotNull(message = "변경할 회원 상태를 선택해주세요.")
-  private MemberState memberState;
-}
