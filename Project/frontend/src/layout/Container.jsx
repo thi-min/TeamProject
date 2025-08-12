46a4a83f8c799e20752801b5d7a10a4d62c82f9c
Nullsec <ejaebok5252@gmail.com>
2025-08-12 12:00:04 +0900

    🐙front chat test

--- Project/frontend/src/layout/Container.jsx
+++ Project/frontend/src/layout/Container.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+//import React from 'react';
 
-import { Link } from 'react-router-dom'; // 페이지 이동용
+// import { Link } from 'react-router-dom'; // 페이지 이동용
 
 const Container = ({ children }) => {
   return <div id="container">{children}</div>;
