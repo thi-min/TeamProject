9a5a5509972725474b8ba4e850898693eab5d1a7
shallow960 <dksgudwn45@gmail.com>
2025-08-29 10:04:57 +0900

    memberServiceImpl 인식오류 해결

--- Project/backend/src/main/java/com/project/member/service/MemberServiceImpl.java
+++ Project/backend/src/main/java/com/project/member/service/MemberServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
-
+ 
 import com.project.common.jwt.JwtTokenProvider;
 import com.project.common.util.JasyptUtil;
 import com.project.member.dto.AddressUpdateRequestDto;
